dd1f74b872134abffc6f55f0b7350c7ddf5645cb
SUK TARA <suktara447@gmail.com>
2024-10-09 12:56:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    
+   <h1 className='text-center bg-teal-200 py-10 text-3xl font-bold'>Life is Beautiful</h1>
+    
+  )
+}
+
+export default App
